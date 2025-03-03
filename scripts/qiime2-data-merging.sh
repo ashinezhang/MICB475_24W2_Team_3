7ab91ee639869438b46bcefea0219710d404ce4d
ashinezhang <99300222+ashinezhang@users.noreply.github.com>
2025-03-03 14:10:56 -0800

    Update qiime2-data-merging.sh

--- scripts/qiime2-data-merging.sh
+++ scripts/qiime2-data-merging.sh
@@ -1,5 +1,6 @@
 # Processing and merging Colombia and Mexico Diabetes datasets using QIIME2 for downstream analysis
 
+# MARCH 1
 # Create a directory for the project and navigate to it - this will be the working directory
 mkdir /data/diabetes
 cd /data/diabetes
@@ -17,4 +18,69 @@ qiime tools import \
   --input-format PairedEndFastqManifestPhred33V2 \
   --input-path /mnt/datasets/project_2/diabetes/mexico_manifest.tsv \
   --output-path ./mexico_demux_seqs.qza
+
+# MARCH 3
+# Create visualization of demultiplexed samples
+qiime demux summarize \
+  --i-data mexico_demux_seqs.qza \
+  --o-visualization mexico_demux_seqs.qzv
+
+qiime demux summarize \
+  --i-data colombia_demux_seqs.qza \
+  --o-visualization colombia_demux_seqs.qzv
+  
+# Make directory in home directory on local machine
+# Copy file
+scp root@<IP>:/data/diabetes/mexico_demux_seqs.qzv . 
+scp root@<IP>:/data/diabetes/colombia_demux_seqs.qzv . 
+
+# chose truncation of 220
+
+# MARCH ??
+# Denoise: Determine ASVs with DADA2
+qiime dada2 denoise-paired \
+  --i-demultiplexed-seqs mexico_demux_seqs.qza \
+  --p-trim-left-f 0 \
+  --p-trunc-len-f 220 \
+  --p-trim-left-r 0 \
+  --p-trunc-len-r 220 \
+  --o-representative-sequences mexico-rep-seqs.qza \
+  --o-table mexico-table.qza \
+  --o-denoising-stats mexico-stats.qza
+
+qiime dada2 denoise-single \
+  --i-demultiplexed-seqs colombia_demux_seqs.qza \
+  --p-trim-left 0 \ 
+  --p-trunc-len 220 \ 
+  --o-representative-sequences colombia-rep-seqs.qza \
+  --o-table colombia-table.qza \
+  --o-denoising-stats colombia-stats.qza
+  
+# Visualize DADA2 stats
+qiime metadata tabulate \
+  --m-input-file mexico-stats.qza \
+  --o-visualization mexico-stats.qzv
+
+qiime metadata tabulate \
+  --m-input-file colombia-stats.qza \
+  --o-visualization colombia-stats.qzv
+
+# Visualize ASVs stats
+qiime feature-table summarize \
+  --i-table mexico-table.qza \
+  --o-visualization mexico-table.qzv \
+  --m-sample-metadata-file /mnt/datasets/project_2/diabetes/mexico_metadata.tsv
+
+qiime feature-table summarize \
+  --i-table colombia-table.qza \
+  --o-visualization colombia-table.qzv \
+  --m-sample-metadata-file /mnt/datasets/project_2/colombia/colombia_metadata.txt
+  
+qiime feature-table tabulate-seqs \
+  --i-data mexico-rep-seqs.qza \
+  --o-visualization mexico-rep-seqs.qzv
+
+qiime feature-table tabulate-seqs \
+  --i-data colombia-rep-seqs.qza \
+  --o-visualization colombia-rep-seqs.qzv
 
