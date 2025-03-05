c5ecc1e5e32263128a3bf25b11f631af4bdc2648
ashinezhang <99300222+ashinezhang@users.noreply.github.com>
2025-03-05 08:20:55 -0800

    Update qiime2-data-merging.sh
    Added classifier training code

--- scripts/qiime2-data-merging.sh
+++ scripts/qiime2-data-merging.sh
@@ -1,25 +1,25 @@
 # Processing and merging Colombia and Mexico Diabetes datasets using QIIME2 for downstream analysis
 
-# MARCH 1
-# Create a directory for the project and navigate to it - this will be the working directory
+## MARCH 1
+# Create a directory for the project and navigate to it - this will be the working directory for separately processing the datasets 
 mkdir /data/diabetes
 cd /data/diabetes
 
-# Import the Colombia dataset using the manifest file
+# Import the Colombia dataset using the manifest file and SingleEnd code
 qiime tools import \
   --type "SampleData[SequencesWithQuality]" \
   --input-format SingleEndFastqManifestPhred33V2 \
   --input-path /mnt/datasets/project_2/colombia/colombia_manifest.txt \
   --output-path ./colombia_demux_seqs.qza
 
-# Import the Mexico Diabetes dataset using the manifest file
+# Import the Mexico Diabetes dataset using the manifest file and PairedEnd code
 qiime tools import \
   --type "SampleData[PairedEndSequencesWithQuality]" \
   --input-format PairedEndFastqManifestPhred33V2 \
   --input-path /mnt/datasets/project_2/diabetes/mexico_manifest.tsv \
   --output-path ./mexico_demux_seqs.qza
 
-# MARCH 3
+## MARCH 3
 # Create visualization of demultiplexed samples
 qiime demux summarize \
   --i-data mexico_demux_seqs.qza \
@@ -28,23 +28,13 @@ qiime demux summarize \
 qiime demux summarize \
   --i-data colombia_demux_seqs.qza \
   --o-visualization colombia_demux_seqs.qzv
-  
-# Make directory in home directory on local machine
-# Copy file
+
+# Make directory in home directory on local machine and copy file
 scp root@<IP>:/data/diabetes/mexico_demux_seqs.qzv . 
 scp root@<IP>:/data/diabetes/colombia_demux_seqs.qzv . 
+# use https://view.qiime2.org/ to visualize and chose truncation of 220
 
-# chose truncation of 220
-
-# MARCH ??
-
-  # Maybe use this to split forward and reverse for Colombia
-  qiime demux split-paired \
-  --i-demux demux-paired.qza \
-  --o-demux-forward only-forward-reads.qza \
-  --o-demux-reverse only-reverse-reads.qza
-
-
+## MARCH 4, code took very long to load
 # Denoise: Determine ASVs with DADA2
 qiime dada2 denoise-paired \
   --i-demultiplexed-seqs mexico_demux_seqs.qza \
@@ -92,8 +82,8 @@ qiime feature-table tabulate-seqs \
   --i-data colombia-rep-seqs.qza \
   --o-visualization colombia-rep-seqs.qzv
 
-##Merge Datasets Together
-#Make and change directory to merged
+## Merge Datasets Together
+# Make and change directory to /data/diabetes/merged_data, this will be the working directory for the merged datasets
 mkdir merged_data
 cd merged_data
 
@@ -109,7 +99,34 @@ qiime feature-table merge-seqs \
  --i-data /data/diabetes/colombia-rep-seqs.qza \
  --o-merged-data merged_rep-seqs.qza
 
-##Taxonomy Analysis??
+## Train classifier on the merged datasets because there are only primer sequences for Colombia dataset and they are both V4 region 
+  # CODE FROM CANVAS, NEED TO EDIT
+  # Extract your amplicon of interest from the reference database
+  # replace the ref-otus.qza with the representative sequence file on the server (e.g. /mnt/datasets/silva_ref_files/silva-138-99-seqs.qza)
+  # replace primer sequences with your correct sequences
+  # replace trunc-len with the one you defined in your denoising step
+  qiime feature-classifier extract-reads \
+    --i-sequences ref-otus.qza \ # merged_rep-seqs.qza?
+    --p-f-primer GTGCCAGCMGCCGCGGTAA \ 
+    --p-r-primer GGACTACHVGGGTWTCTAAT \
+    --p-trunc-len 220 \
+    --o-reads ref-seqs-trimmed.qza
+  
+  # Train classifier with your new ref-seq file
+  # Replace ref-taxonomy.qza with the representative taxonomy file on the server (e.g. /mnt/datasets/silva_ref_files/silva-138-99-tax.qza)
+  qiime feature-classifier fit-classifier-naive-bayes \
+    --i-reference-reads ref-seqs-trimmed.qza \
+    --i-reference-taxonomy ref-taxonomy.qza \ # i have no idea what this would be, don't we need the classifer to make the taxonomy?
+    --o-classifier classifier.qza
+  
+  # Use the trained classifier to assign taxonomy to your reads (rep-seqs.qza)
+  qiime feature-classifier classify-sklearn \
+    --i-classifier classifier.qza \
+    --i-reads merged_rep-seqs.qza \
+    --o-classification merged_taxonomy.qza
+
+
+## Taxonomy Analysis
   qiime feature-classifier classify-sklearn \
   --i-classifier /mnt/datasets/classifiers/silva-138-99-nb-classifier.qza \  ###change potentially
   --i-reads merged_rep-seqs.qza \
