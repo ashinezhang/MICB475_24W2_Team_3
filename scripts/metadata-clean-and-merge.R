150067380cb531d5825a7d3286c0a1e86727cbea
Jhone-applesauce <124637915+Jhone-applesauce@users.noreply.github.com>
2025-03-08 14:02:16 -0800

    Adjusted the code to generate a tsv and changed sampelID column name

--- scripts/metadata-clean-and-merge.R
+++ scripts/metadata-clean-and-merge.R
@@ -25,13 +25,14 @@ col_selected <- col_meta_diabetes %>%
 Group_counts <- col_selected%>%
   count(Group)
 
+##March 8th 2025 - This section was adjusted to meet requirments for qiime2. Namely changed Sample_ID column name to sample-id and generated a tsv instead of a csv
 # Adjust Weight classification and column names
 
 col_merg <- col_selected %>%
   mutate(BMI_class = ifelse(BMI <= 25, "lean", 
                             ifelse(BMI >25 & BMI <= 30, "overweight", 
                                    ifelse(BMI >30 & BMI <=35, "obese", "severe"))))
-colnames(col_merg) <- c("Sample_ID", "Age", "BMI", "Country", "Sex", "Group", "BMI_class")
+colnames(col_merg) <- c("sample-id", "Age", "BMI", "Country", "Sex", "Group", "BMI_class")
 
 #Add country to Mexico dataset
 mex_meta$Country <- "Mexico"
@@ -42,10 +43,13 @@ mex_meta<-rename(mex_meta, Sample=Sample_ID,Sample_ID=sample.id, BMI_class=Obesi
 #Make sex lowercase to match with Colombia dataset
 mex_meta$Sex <- tolower(mex_meta$Sex)
 
+
 #Select columns and merge datasets
 mex_selected <- mex_meta %>%
-  select(c("Sample_ID", "Age", "BMI", "Country", "Sex", "Group", "BMI_class"))
+  select(c("sample-id", "Age", "BMI", "Country", "Sex", "Group", "BMI_class"))
 
 diabetes_merged <- bind_rows(col_merg, mex_selected)
 
-write.csv(diabetes_merged, "./data/merged_diabetes_metadata.csv", row.names = FALSE, col.names = TRUE)
+##old code: write.csv(diabetes_merged, "./data/merged_diabetes_metadata.csv", row.names = FALSE, col.names = TRUE)
+##new code:
+write.table(diabetes_merged, file = "new_merged_diabetes_metadata.csv", sep = "\t", row.names = FALSE, col.names = TRUE)
