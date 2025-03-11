386d13f847036b55500a760c7803c42a9365794a
ashinezhang <99300222+ashinezhang@users.noreply.github.com>
2025-03-10 22:38:43 -0700

    Create colombia-qiime2-analysis.sh

---
+++ scripts/colombia-qiime2-analysis.sh
@@ -0,0 +1,67 @@
+# We already have colombia_demux_seqs.qza, colombia-rep-seqs.qza, colombia-table.qza, colombia-stats.qza and the respective .qzv files completed 
+# ^ Code is in the /scripts/qiime2-data-merging.sh file
+# Now that we abandoned the Mexico diabetes dataset due to large regional differences, we need to do the analysis on just the Colombia dataset
+
+# make a new working directory to keep it separate from the merged analyses
+mkdir /data/diabetes/colombia
+cd /data/diabetes/colombia
+
+## Import new_colombia_diabetes_metadata.tsv from local computer Github to the server
+scp new_merged_diabetes_metadata.csv root@10.19.139.163:/data/diabetes/colombia
+
+
+##### CODE NOT RUN YET, JUST DRAFTED 
+##### NEED TO CHECK FILE PATHS ARE CORRECT, MAYBE MOVE COLOMBIA FILES INTO THE COLOMBIA FOLDER?
+
+
+## Taxonomy Analysis
+##Skip training classifier and use provided classifier to assign taxonomy to your reads since they are V4 region
+qiime feature-classifier classify-sklearn \
+  --i-classifier /mnt/datasets/classifiers/silva-138-99-515-806-nb-classifier.qza \
+  --i-reads /data/diabetes/colombia_rep-seqs.qza \
+  --o-classification colombia_taxonomy.qza
+
+qiime metadata tabulate \
+ --m-input-file colombia_taxonomy.qza \
+ --o-visualization colombia_taxonomy.qzv
+
+## Create taxa barplot
+qiime taxa barplot \
+ --i-table /data/diabetes/colombia-table.qza \
+ --i-taxonomy colombia_taxonomy.qza \
+ --m-metadata-file /data/diabetes/colombia/new_colombia_diabetes_metadata.tsv \  
+ --o-visualization colombia_taxa-bar-plots.qzv
+
+
+###Filtering
+#Filter out mitochondria and chloroplast data
+qiime taxa filter-table \
+  --i-table /data/diabetes/colombia-table.qza \
+  --i-taxonomy colombia_taxonomy.qza \
+  --p-exclude mitochondria,chloroplast \
+  --o-filtered-table table-no-mitochondria-no-chloroplast.qza
+
+#Visualization of table
+qiime feature-table summarize \
+  --i-table table-no-mitochondria-no-chloroplast.qza \
+  --o-visualization table-no-mitochondria-no-chloroplast.qzv \
+  --m-sample-metadata-file /data/diabetes/colombia/new_colombia_diabetes_metadata.tsv
+
+
+# Generate a tree for phylogenetic diversity analyses
+qiime phylogeny align-to-tree-mafft-fasttree \
+  --i-sequences /data/diabetes/colombia_rep-seqs.qza \
+  --o-alignment aligned-rep-seqs.qza \
+  --o-masked-alignment masked-aligned-rep-seqs.qza \
+  --o-tree unrooted-tree.qza \
+  --o-rooted-tree rooted-tree.qza 
+
+# Alpha-rarefaction - need to adjusted depth
+qiime diversity alpha-rarefaction \
+  --i-table /data/diabetes/colombia-table.qza \
+  --i-phylogeny rooted-tree.qza \
+  --p-max-depth 25000 \
+  --m-metadata-file /data/diabetes/colombia/new_colombia_diabetes_metadata.tsv\
+  --o-visualization alpha-rarefaction.qzv
+
+  
