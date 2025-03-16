12a12ef4412df4f307bdf8bb0485f6d50d3b4545
ktien89 <kristentien8902@gmail.com>
2025-03-16 10:12:22 -0700

    update

--- scripts/Phyloseq_obj.R
+++ scripts/Phyloseq_obj.R
@@ -4,16 +4,16 @@ library(tidyverse)
 library(vegan)
 
 #### Load data ####
-metafp <- "colombia_export/metadata.tsv"
+metafp <- "data/colombia_export/metadata.tsv"
 meta <- read_delim(metafp, delim="\t")
 
-otufp <- "colombia_export/feature-table.txt"
+otufp <- "data/colombia_export/feature-table.txt"
 otu <- read_delim(file = otufp, delim="\t", skip=1)
 
-taxfp <- "colombia_export/taxonomy.tsv"
+taxfp <- "data/colombia_export/taxonomy.tsv"
 tax <- read_delim(taxfp, delim="\t")
 
-phylotreefp <- "colombia_export/tree.nwk"
+phylotreefp <- "data/colombia_export/tree.nwk"
 phylotree <- read.tree(phylotreefp)
 
 #### Format OTU table ####
@@ -38,4 +38,20 @@ TAX <- tax_table(tax_mat)
 #### Create phyloseq object ####
 # Merge all into a phyloseq object
 colombia<- phyloseq(OTU, SAMP, TAX, phylotree)
+
+###Filtering Steps###
+# Remove non-bacterial sequences, if any
+colombia_filt <- subset_taxa(colombia,  Domain == "d__Bacteria" & Class!="c__Chloroplast" & Family !="f__Mitochondria")
+# Remove ASVs that have less than 5 counts total
+colombia_filt_nolow <- filter_taxa(colombia_filt, function(x) sum(x)>5, prune = TRUE)
+# Remove samples with less than 100 reads
+colombia_final<- prune_samples(sample_sums(colombia_filt_nolow)>100, colombia_filt_nolow)
+
+###Rarefaction###
+rarecurve(t(as.data.frame(otu_table(colombia_final))), cex=0.1)
+colombia_rare <- rarefy_even_depth(colombia_final, rngseed = 1, sample.size = 5900)
+
+##### Saving #####
+save(colombia_final, file="data/colombia_final.RData")
+save(colombia_rare, file="data/colombia_rare.RData")
 
